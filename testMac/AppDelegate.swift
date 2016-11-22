c171095c4dcbbd2e62225e3d4a696ff37fdf3993
刘海 <liuhai@liuhaidembp.lan>
2016-11-22 10:55:29 +0800

    Initial Commit

---
+++ testMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  testMac
+//
+//  Created by 刘海 on 2016/11/22.
+//  Copyright © 2016年 刘海. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ testMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testMac
+//
+//  Created by 刘海 on 2016/11/22.
+//  Copyright © 2016年 刘海. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
